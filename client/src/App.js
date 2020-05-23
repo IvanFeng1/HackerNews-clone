1ab17007eba4be65c157f73cb9b0c110a236bb2d
Ivan Feng <feng.ivan.1235@gmail.com>
2020-05-23 14:27:19 -0400

    start

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div>
+      alkdsjfasldkfj
+      <div>a;ldskjf</div>
+    </div>
+  );
+}
+
+export default App;
