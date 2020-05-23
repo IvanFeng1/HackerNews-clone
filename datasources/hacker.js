2812da73c9051bea600e23dcc6f115c576e44ec4
Ivan Feng <feng.ivan.1235@gmail.com>
2020-05-23 18:20:45 -0400

    simplified version of the apolloserver

---
+++ datasources/hacker.js
@@ -0,0 +1,32 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+class HackerAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://hacker-news.firebaseio.com/v0/";
+  }
+
+  async postConverter(id) {
+    // converts post ID to a post object
+    const postResponse = await this.get(`item/${id}.json?print=pretty`);
+    return {
+      title: postResponse.title,
+      user: postResponse.by,
+      url: postResponse.url,
+    };
+  }
+
+  async getTopStories() {
+    const response = await this.get("topstories.json?print=pretty"); // getting the list of IDs that correspond to the top stories on hackernews at the time
+    var postArray = [];
+    // if (Array.isArray(response)) {
+    for (var i = 0; i < 15; i++) {
+      var post = await this.postConverter(response[i]);
+      postArray.push(post);
+    }
+    // }
+    return postArray;
+  }
+}
+
+module.exports = HackerAPI;
