210e5095011d3a017ceda752362c85e48115b30e
Ivan Feng <feng.ivan.1235@gmail.com>
2020-05-27 21:44:39 -0400

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Fragment } from "react";
+import { Waypoint } from "react-waypoint";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+// makeStyles for styling components, CssBaseline because it makes site look nice
+// Container for maxwidth
+import { makeStyles, Button, CssBaseline, Container } from "@material-ui/core/";
+
+// components
+import Homepage from "./components/Homepage.js";
+const get_top_posts = gql`
+  query postList($after: Int) {
+    posts(after: $after) {
+      cursor
+      hasMore
+      posts {
+        title
+        id
+        user
+        url
+        cursor
+      }
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div>
-      alkdsjfasldkfj
-      <div>a;ldskjf</div>
-    </div>
-  );
+  return <Homepage />;
 }
 
 export default App;
