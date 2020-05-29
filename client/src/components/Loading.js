9e30017c76b1b22cea47638e6a0df83ca1325950
Ivan Feng <feng.ivan.1235@gmail.com>
2020-05-29 19:03:00 -0400

    centered everything

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -5,6 +5,9 @@ import { Helmet } from "react-helmet"; // need this for set the background colou
 
 //loading animation
 import * as loadingAnimation from "./loadingAnimation.json";
+
+// material ui element
+import { Typography, Box, makeStyles } from "@material-ui/core";
 const defaultOptions = {
   loop: true,
   autoplay: true,
@@ -14,20 +17,58 @@ const defaultOptions = {
   },
 };
 
-// const styles = {
-//   body: {
-//     background: ,
-//   },
-// };
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+  },
+  loadingText: {
+    margins: "3em",
+  },
+});
 function Loading() {
+  const classes = useStyles();
   return (
     <div className="application">
       <Helmet>
         <style>{"body { background-color: #2E3047; }"}</style>
       </Helmet>
-      <FadeIn>
-        <Lottie options={defaultOptions} width={550} length={550} />
-      </FadeIn>
+      <Box
+        display="flex"
+        alignItems="center" // aligns item vertically
+        justifyContent="center" // aligns item horizontally
+        flexWrap="wrap"
+        p={1} // padding
+        m={1} // margins
+        // the props inside css made the box component take up the entire background
+        css={{
+          position: "absolute",
+          top: "0",
+          right: "0",
+          bottom: "0",
+          left: "0",
+        }}
+        flexDirection="column"
+      >
+        <Box>
+          <Lottie options={defaultOptions} width={120} length={120} />
+          {/*the animation component */}
+        </Box>
+        <Box m={2}>
+          <Typography variant="h6" style={{ color: "#dedede" }}>
+            Loading...
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box display="flex" alignItems="center"></Box>
+      {/* <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        m={2}
+      >
+      </Box> */}
     </div>
   );
 }
