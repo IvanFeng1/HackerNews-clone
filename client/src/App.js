e612a11312f4215e4e5860602cec20aa61c4eea1
Ivan Feng <feng.ivan.1235@gmail.com>
2020-06-09 10:12:20 -0400

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,18 @@
-import React, { Fragment } from "react";
-import { Waypoint } from "react-waypoint";
-import { useQuery } from "@apollo/react-hooks";
-import gql from "graphql-tag";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
 
-// makeStyles for styling components, CssBaseline because it makes site look nice
-// Container for maxwidth
-import { makeStyles, Button, CssBaseline, Container } from "@material-ui/core/";
-
-// components
-import Homepage from "./components/Homepage.js";
-const get_top_posts = gql`
-  query postList($after: Int) {
-    posts(after: $after) {
-      cursor
-      hasMore
-      posts {
-        title
-        id
-        user
-        url
-        cursor
-      }
-    }
-  }
-`;
-
+// different pages for the site
+import Homepage from "./pages/Homepage.js";
+import Commentpage from "./pages/Commentpage.js";
 function App() {
-  return <Homepage />;
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/items/:id" component={Commentpage} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
