29553c8c116b79b005bfdd8d31fea022747c7fbc
Ivan Feng <feng.ivan.1235@gmail.com>
2020-07-19 17:54:35 -0400

    Set up to connect to Apollo studio

---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  service: {
+    name: "apollo-server-33334",
+    endpoint: {
+      url: "http://localhost:4000/graphql",
+      skipSSLValidation: true, // optional, disables SSL validation check
+    },
+  },
+};
--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 const HackerAPI = require("./datasources/hacker.js");
 const resolvers = require("./resolvers.js");
+
+require("dotenv").config();
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => ({
     hackerAPI: new HackerAPI(),
   }),
+  engine: {
+    apiKey: process.env.APOLLO_KEY,
+    graphVariant: process.env.NODE_ENV,
+    reportSchema: true,
+  },
+  cors: true,
 });
 
-server.listen().then(({ url }) => {
-  console.log(url);
-});
+// server.listen().then(({ url }) => {
+//   console.log(url);
+// });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
